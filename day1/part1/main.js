e1106957810388cb2c3fffda6e651acb0f33d1a6
unclamped <clear6860@tutanota.com>
2023-12-01 14:50:49 -0300

    i fucking hated this so much
    i was happy, happy as fucking fuck, until I realized in part 2 that my solution was wrong. I checked and checked and couldn't realize why, until I saw in the input something. `oneight`. I wondered "wait, is this supposed to be 1, 8 or 1?
    after asking, the worst outcome came true. it was 1 8. at the time I was using regex, so I started looking for ways of somehow making the regex match overlaps. that took me probably an hour or two, and then after I found how it was supposed to be done (capture group lookahead), it worked in the sense of it detecting the overlapping numbers through an array, but it would return them as fucking null. i spent probably another hour trying to figure out why, and then I gave up on regex altogether.
    another hour or two, and I made the monster that is part2's code. regex by fucking hand, iterating through each single fucking character by myself, i don't even care about trying to make this look pretty or polished or finished or optimized, I'm fucking done with this exercise. i am completing the rest of this advent because I am not a pussy, but MY FUCKING GOD if the rest of the exercises end up being this same shit, I am not touching it with a pole ever again.
    the worst part is that not even the examples in the aoc page accounted for this, so they never made an example where you had something like `oneight`, that was a big mistake.
    i am also not ever using javascript again on this advent, fuck you js.

---
+++ day1/part1/main.js
@@ -0,0 +1,15 @@
+import { open } from 'node:fs/promises';
+
+(async () => {
+    const file = await open('./input');
+
+    let calibrationSum = 0;
+  
+    for await (const line of file.readLines()) {
+      let processedLine = line.match(/[0-9]/g);
+      let lineValue = processedLine[0] + processedLine[processedLine.length - 1];
+      calibrationSum += parseInt(lineValue);
+    }
+
+    console.log(calibrationSum)
+})();
---
+++ day1/part2/main.js
@@ -0,0 +1,95 @@
+import { open } from 'node:fs/promises';
+
+(async () => {
+    const file = await open('./input');
+
+    let calibrationSum = 0;
+
+    const numbersDict = {
+      'one': '1',
+      'two': '2',
+      'three': '3',
+      'four': '4',
+      'five': '5',
+      'six': '6',
+      'seven': '7',
+      'eight': '8',
+      'nine': '9',
+    }
+
+    const numbers = [
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '0'
+    ]
+  
+    for await (const line of file.readLines()) {
+
+
+      let numbersArray = [];
+      let numbersCleanArray = [];
+
+
+      let letters = "";
+      caracteres:
+      for (let i = 0; i < line.length; i++) {      //     por cada caracter que haya en la linea, izq a der
+        letters += line[i];                        //     agregar este caracter
+        // console.log(letters)                       //     print los caracteres acumuladas
+        for (let i = 0; i < numbers.length; i++) { //     por cada elemento de letras, one, 1, etc
+          if (letters.includes(numbers[i])) {      //     si es que las letras acumuladas coinciden con este elemento
+            numbersArray.push(numbers[i])     //     pushear al clean array el numero que conseguimos
+            letters = "";
+            break caracteres;                      //     romper el loop de los caracteres
+          };
+        }
+      }
+      caracteres2:
+      for (let i = line.length - 1; i >= 0; i--) { //     por cada caracter que haya en la linea, der a izq
+        letters += line[i];                        //     agregar este caracter
+        // console.log(letters)                       //     print los caracteres acumuladas
+
+        let lettersArr = letters.split("")
+        let lettersArrRev = lettersArr.reverse()
+        let lettersRev = lettersArrRev.join("")
+
+        for (let i = 0; i < numbers.length; i++) { //     por cada elemento de letras, one, 1, etc
+          if (lettersRev.includes(numbers[i])) {      //     si es que las letras acumuladas coinciden con este elemento
+            numbersArray.push(numbers[i])     //     pushear al clean array el numero que conseguimos
+            letters = "";
+            break caracteres2;                     //     romper el loop de los caracteres
+          };
+        }
+      }
+
+      for (let i = 0; i < numbersArray.length; i++) {
+        if (numbersArray[i].match(/\d/)) {
+          numbersCleanArray.push(numbersArray[i]);
+          continue;
+        }
+
+        numbersCleanArray.push(numbersDict[numbersArray[i]]);
+      }
+      console.log(numbersCleanArray)
+
+      let lineValue = numbersCleanArray[0] + numbersCleanArray[numbersCleanArray.length - 1];
+      calibrationSum += parseInt(lineValue);
+    }
+
+    console.log(calibrationSum);
+})();
